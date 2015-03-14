5880d43cf9f4a0cd3bb7b0fecf08e56e32952004
Bernhard Feldbacher <bernhard.feldbacher@stud.sbg.ac.at>
2015-03-14 08:23:23 +0100

    Add: wrapper functions with normalized interface for decoding and encoding.

--- test_bitrate.c
+++ test_bitrate.c
@@ -6,7 +6,14 @@
  * 2nd wildcard is output file (j2k)
  * 3rd wildcard is quality setting e.g. 30,40,50. Must be increasing 1st < 2nd < 3rd layer.
  */
-char path_jpeg2000[] = "./lib/openjpeg-2.1.0/bin/opj_compress -i %s -o %s -q %s";
+char path_jpeg2000_enc[] = "./lib/openjpeg-2.1.0/bin/opj_compress -i %s -o %s -q %s";
+
+/**
+ * Command to decompress JPEG2000
+ * 1st wildcard is input file (j2k)
+ * 2nd wildcard is output file (bmp)
+ */
+char path_jpeg2000_dec[] = "./lib/openjpeg-2.1.0/bin/opj_decompress -i %s -o %s";
 
 /**
  * Command to compress JPEG XR
@@ -14,7 +21,14 @@ char path_jpeg2000[] = "./lib/openjpeg-2.1.0/bin/opj_compress -i %s -o %s -q %s"
  * 2nd wildcard is output file (jxr)
  * 3rd wildcard is quality setting 0.0 - 1.0
  */
-char path_jpegxr[] = "./lib/jxrlib-master/JxrEncApp -i %s -o %s -q %s";
+char path_jpegxr_enc[] = "./lib/jxrlib-master/JxrEncApp -i %s -o %s -q %f";
+
+/**
+ * Command to decompress JPEG XR
+ * 1st wildcard is input file (jxr)
+ * 2nd wildcard is output file (bmp)
+ */
+char path_jpegxr_dec[] = "./lib/jxrlib-master/JxrDecApp -i %s -o %s";
 
 /**
  * Command to compress JPEGv9
@@ -22,7 +36,125 @@ char path_jpegxr[] = "./lib/jxrlib-master/JxrEncApp -i %s -o %s -q %s";
  * 2nd wildcard is output file (jpg)
  * 3rd wildcard is input file (bmp)
  */
-char path_jpeg[] = "./lib/jpeg-9a/cjpeg -quality %s -outfile %s %s";
+char path_jpeg_enc[] = "./lib/jpeg-9a/cjpeg -quality %d -outfile %s %s";
+
+
+/**
+ * Command to decompress JPEGv9
+ * 1st wildcard is output file (bmp)
+ * 2nd wildcard is input file (jpg)
+ */
+char path_jpeg_dec[] = "./lib/jpeg-9a/djpeg -outfile %s %s";
+
+/**
+ * JPEG2000: Function to encode a bmp file to a j2k file with configurable quality.
+ * @param char* in Path to input bmp file
+ * @param char* out Path to output j2k file
+ * @param int quality Something between 1 and 100.
+ * @return int exist status of encoding process.
+ */
+int
+jpeg_2000_enc(char* in, char* out, int quality)
+{
+  char command[1000];
+  char quality2000[15];
+
+  /**
+   * Generate quality setting "a,b,c"
+   * a = 5 - 80, quality * 75 / 100 + 5
+   * b = 10 - 90, quality * 80 / 100 + 10
+   * c = 15 - 100, quality * 85 / 100 + 15
+   */
+  sprintf(quality2000, "%d,%d,%d", (quality * 75 / 100 + 5), (quality * 80 / 100 + 10), (quality * 85 / 100 + 15));
+
+  sprintf(command, path_jpeg2000_enc, in, out, quality2000);
+  printf("exec: %s\n", command);
+  return system(command);
+}
+
+/**
+ * JPEG2000: Function to decode a j2k file to bmp.
+ * @param char* in Path to input j2k file
+ * @param char* out Path to output bmp file
+ * @return int exit status of decoding process.
+ */
+int
+jpeg_2000_dec(char* in, char* out)
+{
+  char command[1000];
+  sprintf(command, path_jpeg2000_dec, in, out);
+  printf("exec: %s\n", command);
+  return system(command);
+}
+
+/**
+ * JPEGXR: Function to encode a bmp file to a jxr file with configurable quality.
+ * @param char* in Path to input bmp file
+ * @param char* out Path to output jxr file
+ * @param int quality Something between 1 and 100.
+ * @return int exist status of encoding process.
+ */
+int
+jpeg_xr_enc(char* in, char* out, int quality)
+{
+  char command[1000];
+
+  /**
+   * Generate quality setting 0.0-1.0
+   * q = (double) quality / 100
+   */
+  sprintf(command, path_jpegxr_enc, in, out, ((double) quality / 100));
+  printf("exec: %s\n", command);
+  return system(command);
+}
+
+/**
+ * JPEGXR: Function to decode a jxr file to bmp.
+ * @param char* in Path to input jxr file
+ * @param char* out Path to output bmp file
+ * @return int exit status of decoding process.
+ */
+int
+jpeg_xr_dec(char* in, char* out)
+{
+  char command[1000];
+  sprintf(command, path_jpegxr_dec, in, out);
+  printf("exec: %s\n", command);
+  return system(command);
+}
+
+/**
+ * JPEGv9: Function to encode a bmp file to a jpg file with configurable quality.
+ * @param char* in Path to input bmp file
+ * @param char* out Path to output jpg file
+ * @param int quality Something between 1 and 100.
+ * @return int exist status of encoding process.
+ */
+int
+jpeg_enc(char* in, char* out, int quality)
+{
+  char command[1000];
+
+  sprintf(command, path_jpeg_enc, quality, out, in);
+  printf("exec: %s\n", command);
+  return system(command);
+}
+
+/**
+ * JPEGv9: Function to decode a jpg file to bmp.
+ * @param char* in Path to input jpg file
+ * @param char* out Path to output bmp file
+ * @return int exit status of decoding process.
+ */
+int
+jpeg_dec(char* in, char* out)
+{
+  char command[1000];
+  sprintf(command, path_jpeg_dec, out, in);
+  printf("exec: %s\n", command);
+  return system(command);
+}
+
 
 /**
  * The main procedure
@@ -41,32 +173,24 @@ main(int argc, const char* argv[])
     return 0;
   }
 
-  // create JPEG2000
-  char output_file2000[] = "out/test.j2k";
-  char quality2000[] = "30,40,50";
-  char command2000[1000];
+  char out_enc_jpg[] = "out/test.jpg";
+  char out_enc_jpg2k[] = "out/test.j2k";
+  char out_enc_jpgxr[] = "out/test.jxr";
 
-  sprintf(command2000, path_jpeg2000, input_file, output_file2000, quality2000);
-  printf("exec: %s\n", command2000);
-  system(command2000);
+  char out_dec_jpg[] = "out/test.jpg.bmp";
+  char out_dec_jpg2k[] = "out/test.j2k.bmp";
+  char out_dec_jpgxr[] = "out/test.jxr.bmp";
 
-  // create JPEG XR
-  char output_filexr[] = "out/test.jxr";
-  char qualityxr[] = "0.5";
-  char commandxr[1000];
+  int q = 5;
 
-  sprintf(commandxr, path_jpegxr, input_file, output_filexr, qualityxr);
-  printf("exec: %s\n", commandxr);
-  system(commandxr);
+  jpeg_enc(input_file, out_enc_jpg, q);
+  jpeg_dec(out_enc_jpg, out_dec_jpg);
 
-  // create JPEGv9
-  char output_file[] = "out/test.jpg";
-  char quality[] = "50";
-  char command[1000];
+  jpeg_2000_enc(input_file, out_enc_jpg2k, q);
+  jpeg_2000_dec(out_enc_jpg2k, out_dec_jpg2k);
 
-  sprintf(command, path_jpeg, quality, output_file, input_file);//
-  printf("exec: %s\n", command);
-  system(command);
+  jpeg_xr_enc(input_file, out_enc_jpgxr, q);
+  jpeg_xr_dec(out_enc_jpgxr, out_dec_jpgxr);
 
   return 0;
 }
